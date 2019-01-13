69697b3dd79a7419cd716144632fef55e59acc6d
Sergey Ganin <mcheguevara2@gmail.com>
2019-01-14 01:31:10 +0300

    fix

---
+++ app/containers/SnakePage.js
@@ -0,0 +1,13 @@
+// @flow
+import React, { Component } from 'react';
+import Snake from '../components/Snake';
+
+type Props = {};
+
+export default class SnakePage extends Component<Props> {
+  props: Props;
+
+  render() {
+    return <Snake />;
+  }
+}
